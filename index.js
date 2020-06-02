cc3a6ca3fdc33d8c62cbf0f12c2a6acf4696dc49
Marc <marc@sommer.work>
2020-06-02 15:16:34 +0200

    added login, registration, logout, packages for authentication

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+"use strict";
 const mongoose = require('mongoose');
 const http = require('http');
 const config = require('./src/config');
-var express = require('express');
-var app = express();
+const api = require('./src/api');
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+// Set the port to the API.
+api.set('port', config.port);
+
+//Create a http server based on Express
+const server = http.createServer(api);
 
 //Deploy nodejs application on port 3000
 //app.listen(3000, function () {
@@ -16,10 +18,19 @@ app.get('/', function (req, res) {
 //Connect to the MongoDB database; then start the server
 mongoose
     .connect(config.mongoURI)
-    .then(() => app.listen(config.port, function () {
+    .then(() => server.listen(config.port, function () {
      	console.log('Machbarschaft app listening on port 3000!');
 		}))
     .catch(err => {
         console.log('Error connecting to the database', err.message);
+        process.exit(err.statusCode);
+    });
+
+server.on('listening', () => {
+        console.log(`API is running in port ${config.port}`);
+    });
+
+server.on('error', (err) => {
+        console.log('Error in the server', err.message);
         process.exit(err.statusCode);
     });
