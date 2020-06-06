82f37b119a95e84e98ef4b830f4b2dbd5bc523c5
Janik Müller <janikmueller387@gmail.com>
2020-06-06 19:39:27 +0200

    MB-42 integrate uml class diagram as mongoose schemes

---
+++ src/models/feedback.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+const processFeedbackSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    needContact: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true, discriminatorKey: 'kind' }
+);
+
+const ProcessFeedback = mongoose.model(
+  'ProcessFeedback',
+  processFeedbackSchema
+);
+
+const AudioFeedback = ProcessFeedback.discriminator(
+  'AudioFeedback',
+  {
+    duration: {
+      type: Number, //in ms
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+  { discriminatorKey: 'kind' }
+);
+
+const FormFeedback = ProcessFeedback.discriminator(
+  'FormFeedback',
+  {
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { discriminatorKey: 'kind' }
+);
+
+export { ProcessFeedback, AudioFeedback, FormFeedback };
---
+++ src/models/response.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const statusStages = ['accepted', 'called', 'on-the-way', 'done'];
+
+const responseSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: statusStages,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Response = mongoose.model('Response', responseSchema);
+
+export default Response;
