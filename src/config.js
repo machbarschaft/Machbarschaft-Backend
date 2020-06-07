52fa5aaeb6f11ae59ff077a062929b023102b851
Marc <marc@sommer.work>
2020-06-07 22:30:18 +0200

    removed unnecessary code

--- src/config.js
+++ src/config.js
@@ -13,8 +13,6 @@ const jwt = {
         sameSite: true,
         signed: true,
         secure: false,
-        // see https://stackoverflow.com/questions/8842732/how-to-get-30-days-prior-to-current-date, could be changed but it works
-        //expires: moment().add(30, 'days').format(), //new Date(new Date().setDate(new Date().getDate() + 30))
         expires: new Date(new Date().setDate(new Date().getDate() + 30))
     }
 }
