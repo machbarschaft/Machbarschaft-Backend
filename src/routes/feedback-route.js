615789f88b89cf551213885e55fe3583bc188431
Janik Müller <janikmueller387@gmail.com>
2020-06-28 00:44:49 +0200

    MB-58 add swagger documentation

--- src/routes/feedback-route.js
+++ src/routes/feedback-route.js
@@ -7,16 +7,33 @@ import passport from 'passport';
 
 const router = Router();
 
-router.get(
-  '/',
-  Validator.idValidationRules('sdf'),
-  Validator.cookieValidationRules(),
-  Validator.validate,
-  (req, res) => {
-    res.status(200).send('Hallo freut mich dass es geklappt hat.');
-  }
-);
-
+/**
+ * @swagger
+ * /feedback/{type}/{id}:
+ *   post:
+ *     summary: Create process feedback
+ *     description: Give feedback regarding a certain request/response.
+ *     tags:
+ *       - feedback
+ *     parameters:
+ *      - in: path
+ *        name: type
+ *        type: String
+ *        required: true
+ *      - in: path
+ *        name: id
+ *        type: ObjectId
+ *        required: true
+ *     responses:
+ *       201:
+ *         description: Successfully save feedback.
+ *       401:
+ *          description: Unauthorized to give feedback for this request/response.
+ *       404:
+ *          description: Request not found OR Response not found
+ *       500:
+ *          description: Internal server error
+ */
 router.post(
   '/:type/:id',
   Validator.idValidationRules('id'),
