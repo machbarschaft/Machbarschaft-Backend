7889b8ccb9389b98ab32db4f527ab91c13783945
Janik Müller <janikmueller387@gmail.com>
2020-07-09 16:31:31 +0200

    MB-79 add swagger documentation

--- src/routes/user-route.js
+++ src/routes/user-route.js
@@ -7,6 +7,36 @@ import passport from 'passport';
 
 const router = Router();
 
+/**
+ * @swagger
+ * /user/profile:
+ *  put:
+ *      summary: Update profile
+ *      description: Set new values for forename and surname in your profile.
+ *      tags:
+ *          - user-profile
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/x-www-form-urlencoded:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          forename:
+ *                              type: String
+ *                          surname:
+ *                              type: String
+ *                  example:
+ *                      forename: John
+ *                      surname: Smith
+ *      responses:
+ *          200:
+ *              description: Successfully updated profile.
+ *          404:
+ *              description: Could not find user with given id.
+ *          500:
+ *              description: Internal server error
+ */
 router.put(
   '/profile',
   Validator.cookieValidationRules('jwt'),
